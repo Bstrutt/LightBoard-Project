3a72805b6b8888152e7f3a4536940f8113afb551
Bryce <bstrutt96@gmail.com>
2022-04-11 08:17:18 -0500

    Copy/Paste from
    https://dev.to/ninjabunny9000/let-s-make-a-twitch-bot-with-python-2nd8

---
+++ TwitchIntegrations/SimpleLocationSelection.py
@@ -0,0 +1,45 @@
+#install pipenv with: pipenv install twitchio
+
+import os
+from twitchio.ext import commands
+
+# set up the bot
+bot = commands.Bot(
+    irc_token=os.environ['TMI_TOKEN'],
+    client_id=os.environ['CLIENT_ID'],
+    nick=os.environ['BOT_NICK'],
+    prefix=os.environ['BOT_PREFIX'],
+    initial_channels=[os.environ['CHANNEL']]
+)
+
+@bot.event
+async def event_ready():
+    'Called once when the bot goes online.'
+    print(f"{os.environ['BOT_NICK']} is online!")
+    ws = bot._ws  # this is only needed to send messages within event_ready
+    await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!")
+
+
+@bot.event
+async def event_message(ctx):
+    'Runs every time a message is sent in chat.'
+
+    # make sure the bot ignores itself and the streamer
+    if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
+        return
+
+    await bot.handle_commands(ctx)
+
+    # await ctx.channel.send(ctx.content)
+
+    if 'hello' in ctx.content.lower():
+        await ctx.channel.send(f"Hi, @{ctx.author.name}!")
+
+
+@bot.command(name='test')
+async def test(ctx):
+    await ctx.send('test passed!')
+
+
+if __name__ == "__main__":
+    bot.run()
