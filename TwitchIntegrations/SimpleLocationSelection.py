049f4cc67d603e2e1cf90150b3f15ad54aa3f2b5
bryce <bstrutt96@gmail.com>
2022-04-12 00:56:08 -0500

    Monster of a commit, got the twitch chat location selection up and running

--- TwitchIntegrations/SimpleLocationSelection.py
+++ TwitchIntegrations/SimpleLocationSelection.py
@@ -1,45 +1,88 @@
-#install pipenv with: pipenv install twitchio
 
-import os
 from twitchio.ext import commands
+import re
+from rpi_ws281x import Color, PixelStrip, ws
+from DisplayColorMatrix import display_color_matrix
 
-# set up the bot
-bot = commands.Bot(
-    irc_token=os.environ['TMI_TOKEN'],
-    client_id=os.environ['CLIENT_ID'],
-    nick=os.environ['BOT_NICK'],
-    prefix=os.environ['BOT_PREFIX'],
-    initial_channels=[os.environ['CHANNEL']]
-)
+blue = Color(0,0,255)
+blank = Color(0,0,0)
+green = Color(255,0,0)
+red = Color(0,255,0)
 
-@bot.event
-async def event_ready():
-    'Called once when the bot goes online.'
-    print(f"{os.environ['BOT_NICK']} is online!")
-    ws = bot._ws  # this is only needed to send messages within event_ready
-    await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!")
+class Bot(commands.Bot):
 
+    def __init__(self):
+        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
+        # prefix can be a callable, which returns a list of strings or a string...
+        # initial_channels can also be a callable which returns a list of strings...
+        super().__init__(token='rejfa91ji58uty2kg9bnt50lppdz7y', prefix='!', initial_channels=['fluxsb'])
+        self.single = False
+        self.colorBoard =  [[0 for j in range(12)] for i in range(12)]
+        for i in range(12):
+            for j in range(12):
+                self.colorBoard[i][j] = blank
+        display_color_matrix(self.colorBoard)   
 
-@bot.event
-async def event_message(ctx):
-    'Runs every time a message is sent in chat.'
+        
 
-    # make sure the bot ignores itself and the streamer
-    if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
-        return
+        
+    async def event_ready(self):
+        # Notify us when everything is ready!
+        # We are logged in and ready to chat and use commands...
+        print(f'Logged in as | {self.nick}')
 
-    await bot.handle_commands(ctx)
+    async def event_message(self, message):
+        # Messages with echo set to True are messages sent by the bot...
+        # For now we just want to ignore them...
+        if message.echo:
+            return
+        
+        print(message.content)
+        
+        matches = re.findall("\({1}[0-9]+,{1}[0-9]+,{1}(?:red|green|blue|blank){1}\){1}",message.content)
 
-    # await ctx.channel.send(ctx.content)
+        if len(matches) > 0:
+            if self.single:
+                matches = [matches[0]]
+            for match in matches:
+                #[int(s) for s in 
+                coordinates = [int(s)for s in re.findall("\d+", match)]
+                print(coordinates)         
 
-    if 'hello' in ctx.content.lower():
-        await ctx.channel.send(f"Hi, @{ctx.author.name}!")
+                if len(coordinates) > 2:
+                    return
+                if coordinates[0] > 11 or coordinates[0] < 0 or coordinates[1] > 11 or coordinates[1] < 0:
+                    await commands.Context.send('Coords between 0 and 11')
+                    return
+                print(match)
+                color = re.findall("red|green|blue|blank", match)[0]
+                print(color)
+                if color == "red":
+                    self.colorBoard[coordinates[0]][coordinates[1]] = red
+                elif color == "green":
+                    self.colorBoard[coordinates[0]][coordinates[1]] = green
+                elif color == "blue":
+                    self.colorBoard[coordinates[0]][coordinates[1]] = blue
+                elif color == "blank":
+                    self.colorBoard[coordinates[0]][coordinates[1]] = blank
+                    
+            print(self.colorBoard)
+            display_color_matrix(self.colorBoard)   
+        
+        # Since we have commands and are overriding the default `event_message`
+        # We must let the bot know we want to handle and invoke our commands...
+        await self.handle_commands(message)
 
+    @commands.command()
+    async def single(self, ctx: commands.Context):
+        
+        if self.single == False:
+            await ctx.send('single mode activated: one coord at a time.')
+            self.Single = True
+        if self.single == True:
+            await ctx.send('single mode deactivated: separate coords by at least a space')
+            self.single = False
 
-@bot.command(name='test')
-async def test(ctx):
-    await ctx.send('test passed!')
-
-
-if __name__ == "__main__":
-    bot.run()
+bot = Bot()
+bot.run()
+# bot.run(
